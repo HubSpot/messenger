2668aa4640a389749a3c69ed4d8cbf4fb446f058
Zack Bloom <zackbloom@gmail.com>
2013-02-13 21:15:59 -0500

    Add theme and position chooser

--- javascripts/demo.js
+++ javascripts/demo.js
@@ -1,3 +1,36 @@
 $(function(){
   $.globalMessenger().post("Thanks for checking out Messenger!");
+
+  var loc = ['bottom', 'right'];
+  var style = 'future';
+
+  var update = function(){
+    classes = 'messenger-fixed';
+
+    classes += ' messenger-theme-' + style;
+
+    for (var i=0; i < loc.length; i++)
+      classes += ' messenger-on-' + loc[i];
+
+    $.globalMessenger({extraClasses: classes});
+    $._messengerDefaults = {extraClasses: classes};
+  };
+
+  $('.location-selector').locationSelector()
+    .on('update', function(pos){
+      loc = pos;
+
+      update();
+    });
+
+
+  $('.theme-selector').themeSelector({
+    themes: ['future', 'block']
+  }).on('update', function(theme){
+    style = theme;
+
+    update();
+  });
+
+
 });

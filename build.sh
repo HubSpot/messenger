4d1a16156cb14cd2f69c76e41717b6b273612db4
Zack Bloom <zackbloom@gmail.com>
2013-02-27 02:55:25 -0500

    Add version information to built js

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 coffee -o build/js src/coffee
 
 compass compile . --sass-dir "src/sass" --css-dir "build/css" --javascripts-dir "build/js" --images-dir "build/images"
+
+echo "// Version `cat VERSION`\n// Built On `date`" | cat - build/js/messenger.js > tmp && mv tmp build/js/messenger.js

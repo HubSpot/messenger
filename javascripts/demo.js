fcf96f20f167ae1e0ec134169317e6ed466c5bbc
Adam Schwartz <aschwartz@hubspot.com>
2013-02-27 03:08:13 -0500

    demo js cleanup

--- javascripts/demo.js
+++ javascripts/demo.js
@@ -9,17 +9,17 @@ $(function(){
   var $tsel = $('.theme-selector');
 
   var update = function(){
-    classes = 'messenger-fixed';
+    var classes = 'messenger-fixed';
 
     classes += ' messenger-theme-' + style;
 
     for (var i=0; i < loc.length; i++)
       classes += ' messenger-on-' + loc[i];
 
-    $.globalMessenger({extraClasses: classes});
-    $._messengerDefaults = {extraClasses: classes};
+    $.globalMessenger({ extraClasses: classes });
+    $._messengerDefaults = { extraClasses: classes };
 
-    $output.text("$._messengerDefaults = {\n\textraClasses: '" + classes + "'\n}")
+    $output.text("$._messengerDefaults = {\n\textraClasses: '" + classes + "'\n}");
   };
 
   update();
@@ -39,6 +39,5 @@ $(function(){
 
     update();
   });
-
 
 });

1c6f4862e7b55407db5fefb9cb6297ec9a0db06a
Joel Moss <joel@developwithstyle.com>
2013-03-05 11:26:25 +0000

    Specs now pass without backbone and underscore

--- src/coffee/libs.js
+++ src/coffee/libs.js
@@ -66,7 +66,7 @@ var _ = (function() {
         var id = ++idCounter + '';
         return prefix ? prefix + id : id;
     };
-        
+
     _.filter = _.select = function(obj, iterator, context) {
         var results = [];
         if (obj == null) return results;
@@ -163,7 +163,7 @@ var Events = (function() {
             }
         };
 
-        var Events = Backbone.Events = {
+        var Events = {
 
             on: function(name, callback, context) {
                 if (!(eventsApi(this, 'on', name, [callback, context]) && callback)) return this;
@@ -217,7 +217,7 @@ var Events = (function() {
 
             trigger: function(name) {
                 if (!this._events) return this;
-                var args = slice.call(arguments, 1);
+                var args = Array.prototype.slice.call(arguments, 1);
                 if (!eventsApi(this, 'trigger', name, args)) return this;
                 var events = this._events[name];
                 var allEvents = this._events.all;

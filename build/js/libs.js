52ac030de8c8a59d2fd49b333e4a6d3d00d10981
Zack Bloom <zackbloom@gmail.com>
2013-03-11 14:55:07 -0400

    Specs now pass without backbone and underscore
    Conflicts:
    
    	build/js/messenger.js
    	build/js/messenger.min.js

--- build/js/libs.js
+++ build/js/libs.js
@@ -1,5 +1,5 @@
 // Version 1.0.9
-// Built On Fri Mar  1 01:29:00 EST 2013
+// Built On Mon Mar 11 14:54:44 EDT 2013
 var _ = (function() {
     var ArrayProto = Array.prototype, ObjProto = Object.prototype, FuncProto = Function.prototype;
 
@@ -68,7 +68,7 @@ var _ = (function() {
         var id = ++idCounter + '';
         return prefix ? prefix + id : id;
     };
-        
+
     _.filter = _.select = function(obj, iterator, context) {
         var results = [];
         if (obj == null) return results;
@@ -165,7 +165,7 @@ var Events = (function() {
             }
         };
 
-        var Events = Backbone.Events = {
+        var Events = {
 
             on: function(name, callback, context) {
                 if (!(eventsApi(this, 'on', name, [callback, context]) && callback)) return this;
@@ -219,7 +219,7 @@ var Events = (function() {
 
             trigger: function(name) {
                 if (!this._events) return this;
-                var args = slice.call(arguments, 1);
+                var args = Array.prototype.slice.call(arguments, 1);
                 if (!eventsApi(this, 'trigger', name, args)) return this;
                 var events = this._events[name];
                 var allEvents = this._events.all;
